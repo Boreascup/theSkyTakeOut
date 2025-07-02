65ee18502758d2f269a9875407b65f3ba87ca7a4
boreascup <1812750453@qq.com>
2025-07-02 16:10:57 +0800

    feat:启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -77,7 +77,6 @@ public class EmployeeController {
         return Result.success();
     }
 
-
     @PostMapping
     @ApiOperation(value = "新增员工")
     public Result save(@RequestBody EmployeeDTO employeeDTO) {
@@ -92,5 +91,13 @@ public class EmployeeController {
         log.info("分页查询{}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        log.info("启用禁用员工账号：{}, {}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
     }
 }
