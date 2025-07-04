e702d7744189645b4af48ccc14dd0a4e38e49ffb
boreascup <1812750453@qq.com>
2025-07-04 14:26:13 +0800

    feat:添加套餐、分页查询

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,30 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
+
+public interface SetmealService {
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    void saveWithDish(SetmealDTO setmealDTO);
+
+
+    /**
+     * 根据id查询套餐和关联的菜品数据
+     * @param id
+     * @return
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+}
